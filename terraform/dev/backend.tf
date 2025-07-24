4f88edd267e531aef0827c29a07e983be1930b00
tobby <zidan.guo186@gmail.com>
2025-07-24 19:08:21 +1000

    Configure Terraform S3 backend

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,8 +1,6 @@
-# bucket, dynamodb_table根据实际情况替换
-
 terraform {
   backend "s3" {
-    bucket         = "insightflow-imba-group-state-tobby"
+    bucket         = "insightflow-imba-group-state"
     key            = "state/terraform.tfstate"
     region         = "ap-southeast-2"
     dynamodb_table = "insightflow_imba_group"
