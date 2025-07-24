7c17bd819f216ae75e19338c5cc5b37b9a668b0a
tobby <zidan.guo186@gmail.com>
2025-07-24 15:15:55 +1000

    fix: Add missing outputs to vpc module

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -2,3 +2,28 @@ output "github_action_role_arn" {
   description = "The ARN of the GitHub Action IAM role"
   value       = aws_iam_role.github_action_role.arn
 }
+
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = [aws_subnet.private.id]
+}
+
+output "postgres_security_group_id" {
+  description = "ID of the PostgreSQL security group"
+  value       = aws_security_group.postgres.id
+}
+
+output "lambda_sync_raw_security_group_id" {
+  description = "ID of the Lambda security group for data sync raw"
+  value       = aws_security_group.lambda.id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = [aws_subnet.public.id]
+}
+
+output "bastion_security_group_id" {
+  description = "ID of the Bastion security group"
+  value       = aws_security_group.bastion.id
+}
