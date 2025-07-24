cee25d369c77d45709958b1e3135d3704e8e5009
tobby <zidan.guo186@gmail.com>
2025-07-24 22:08:12 +1000

    Fix RDS username and password variable names in rds.tf

--- terraform/modules/rds_postgresql/rds.tf
+++ terraform/modules/rds_postgresql/rds.tf
@@ -18,8 +18,8 @@ resource "aws_db_instance" "postgres" {
   max_allocated_storage   = 100
   storage_type            = "gp2"
   db_name                 = var.db_name
-  username                = var.db_username
-  password                = var.db_password
+  username                = var.rds_db_username
+  password                = var.rds_db_password
   db_subnet_group_name    = aws_db_subnet_group.postgres.name
   vpc_security_group_ids  = var.postgres_security_group_ids
   multi_az                = false
