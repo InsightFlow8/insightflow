9248460104cd02b12751a9f370e2808725a1d3a8
tobby <zidan.guo186@gmail.com>
2025-07-24 15:07:11 +1000

    fix: Use unique S3 bucket name for Terraform backend

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "insightflow-imba-group-state"
+    bucket         = "insightflow-imba-group-state-tobby"
     key            = "state/terraform.tfstate"
     region         = "ap-southeast-2"
     dynamodb_table = "insightflow_imba_group"
