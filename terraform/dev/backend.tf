e54d82c729ff9c89e4ba22365f0802c65ca4f1d4
tobby <zidan.guo186@gmail.com>
2025-07-24 19:51:19 +1000

    Update Terraform backend and version configuration

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "insightflow-imba-group-state"
+    bucket         = "insightflow-imba-group-state-tobby"
     key            = "state/terraform.tfstate"
     region         = "ap-southeast-2"
-    dynamodb_table = "insightflow_imba_group"
+    dynamodb_table = "insightflow_imba_group_tobby"
     encrypt        = true
   }
 }
