8ee677e9cd8c9befb736b1c7bf307dd1f82085f1
chypwc <chienyeh5@gmail.com>
2025-07-29 02:07:34 +1000

    Comment out crawler in action workflow

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "insightflow-imba-group-state-22"
+    bucket         = "insightflow-imba-group-state"
     key            = "state/terraform.tfstate"
     region         = "ap-southeast-2"
     dynamodb_table = "insightflow_imba_group"
