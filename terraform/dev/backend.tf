323633f4a50a0675c76bef150831eb1d64b7eced
chypwc <chienyeh5@gmail.com>
2025-07-29 01:46:09 +1000

    Add Glue tables and update partition by Lambda batch ingestion. Add secret manager to handle Snowflake credentials

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "insightflow-imba-group-state"
+    bucket         = "insightflow-imba-group-state-22"
     key            = "state/terraform.tfstate"
     region         = "ap-southeast-2"
     dynamodb_table = "insightflow_imba_group"
